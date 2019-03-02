9ae68b655e245262a19d5184b17599133d94143c
Harika <billuriharika2506@gmail.com>
2019-03-01 17:39:32 -0800

    Added Scroll Bar

---
+++ static/js/template.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    // variables
+    var $header_top = $('.header-top');
+    var $nav = $('nav');
+
+
+
+    // toggle menu
+    $header_top.find('a').on('click', function() {
+        $(this).parent().toggleClass('open-menu');
+    });
+
+
+
+    // fullpage customization
+    $('#fullpage').fullpage({
+        sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#348899', '#F2AE72'],
+        sectionSelector: '.vertical-scrolling',
+        slideSelector: '.horizontal-scrolling',
+        navigation: true,
+        slidesNavigation: true,
+        css3: true,
+        controlArrows: false,
+        anchors: ['home', 'ApplicationOverview', 'TuringTest', 'OtherApi', 'OurAlgorithm','LossVisualization','About'],
+        menu: '#menu',
+
+
+    });
+});
